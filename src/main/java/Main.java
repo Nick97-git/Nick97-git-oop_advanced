5399d43384b5846d52c79453e41046ae97278f27
Nick97-git <nickarchangel97@gmail.com>
2020-04-05 07:33:29 +0300

    added checkstyle plugin in pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        RandomGenerator generator = new RandomGenerator();
         List<Shape> shapes = new ArrayList<>();
         shapes.add(new Square());
         shapes.add(new Triangle());
         shapes.add(new Trapeze());
         shapes.add(new Circle());
+        RandomGenerator generator = new RandomGenerator();
         for (int i = 0; i < generator.getRandomNum(100); i++) {
             int index = generator.getRandomNum(shapes.size());
             Shape generatedShape = shapes.get(index).createShape();
